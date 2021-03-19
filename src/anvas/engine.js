33771b2fd08b2c71c835113dbbe4be20f03ee332
bilasyurii <bilas.yurii@gmail.com>
2021-03-20 00:08:28 +0200

    Changed version to 0.2.0.

--- src/anvas/engine.js
+++ src/anvas/engine.js
@@ -24,7 +24,7 @@ export default class Engine {
     this.physics = null;
     this.state = null;
 
-    this.version = '0.1';
+    this.version = '0.2.0';
     this.elapsed = 0;
     this.elapsedMS = 0;
     this.physicsElapsed = 0;
